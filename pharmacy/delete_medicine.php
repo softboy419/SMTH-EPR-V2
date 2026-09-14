65498d135e9a342cbeb396050ba3474496ac54f1
Edmund Appiah <appiahedmund763@gmail.com>
2026-09-14 16:06:36 +0000

    SECURE DELETION OF MEDICINE EDITING

--- pharmacy/delete_medicine.php
+++ pharmacy/delete_medicine.php
@@ -1,15 +1,34 @@
 <?php
 
 include("../config/database.php");
+include("../includes/auth.php");
 
-if(isset($_GET['id'])){
+$id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 
-    $id = $_GET['id'];
-
-    $sql = "DELETE FROM pharmacy WHERE id='$id'";
-
-    mysqli_query($conn,$sql);
-
-    header("Location: pharmacy.php");
-    exit();
+if ($id === false || $id === null || $id <= 0) {
+    die("Invalid medicine ID.");
 }
+
+$stmt = mysqli_prepare($conn, "DELETE FROM pharmacy WHERE id = ?");
+
+if (!$stmt) {
+    die("Unable to process request.");
+}
+
+mysqli_stmt_bind_param($stmt, "i", $id);
+
+if (mysqli_stmt_execute($stmt)) {
+
+    echo "<script>
+        alert('Medicine Deleted Successfully!');
+        window.location='pharmacy.php';
+    </script>";
+
+} else {
+
+    error_log("Medicine deletion failed: " . mysqli_stmt_error($stmt));
+    echo "Unable to delete medicine.";
+}
+
+mysqli_stmt_close($stmt);
+?>
